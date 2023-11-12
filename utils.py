4d3cf164231a9730161aac6f07946ddcfd424b06
lxu-wu <xuwuleijie@gmail.com>
2023-11-12 06:33:50 +0100

    new_function

--- utils.py
+++ utils.py
@@ -31,6 +31,13 @@ def comparer_matrices(matrice1, matrice2):
                 return False
     return True
 
+def change_ones_to_zeros(matrice):
+    for i in range(len(matrice)):
+        for j in range(len(matrice[0])):
+            if matrice[i][j] == 1:
+                matrice[i][j] = 0
+    return matrice
+
 def only_zeros_and_twos(matrice):
     for i in range(len(matrice)):
         for j in range(len(matrice[0])):
